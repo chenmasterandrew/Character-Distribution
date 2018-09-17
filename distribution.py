53b432f2a50cdc7d75629b3d96f7f4202f7324cb
chenmasterandrew <30731383+chenmasterandrew@users.noreply.github.com>
2018-09-17 12:34:35 -0400

    Updated from Brython Server: 9/17/2018 12:34:34 PM

--- distribution.py
+++ distribution.py
@@ -1,6 +1,6 @@
 """
 distribution.py
-Author: <your name here>
+Author: Andrew Chen
 Credit: <list sources used, if any>
 
 Assignment:
@@ -36,3 +36,14 @@ Notice about this example:
   in the text and they are listed in the output in alphabetical order.
 * Letters that do not occur in the text are not listed in the output at all.
 """
+import string
+letters = string.ascii_lowercase
+text = input("Please enter a string of text (the bigger the better): ")
+print ("The distribution of characters in \"{0}\" is:".format(text))
+lowertext = text.lower()
+textlist = list(lowertext)
+for x in textlist:
+    if x
+
+print (textlist)
+print (letters)
